cc36ed72aca4dc5ada9a786a643e7dace0791e29
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-07-30 10:55:53 +0300

    test: add tests and fix test name

--- test/sqlgen.js
+++ test/sqlgen.js
@@ -109,7 +109,7 @@ test.testSync('Select few where avg', (test, { builder, params }) => {
   test.strictSame(params.build(), [3]);
 });
 
-test.testSync('Select few where avg', (test, { builder, params }) => {
+test.testSync('Select few where min', (test, { builder, params }) => {
   builder
     .from('table')
     .select('f1', 'f2')
@@ -120,6 +120,23 @@ test.testSync('Select few where avg', (test, { builder, params }) => {
   test.strictSame(
     query,
     'SELECT "f1", "f2", min("f0") FROM "table" ' +
+      'WHERE "f1" = $1 GROUP BY "f1", "f2"'
+  );
+  test.strictSame(params.build(), [3]);
+});
+
+test.testSync('Select few where two min expr', (test, { builder, params }) => {
+  builder
+    .from('table')
+    .select('f1', 'f2')
+    .where('f1', '=', 3)
+    .groupBy('f1', 'f2')
+    .min('f0')
+    .min('f3 / f0');
+  const query = builder.build();
+  test.strictSame(
+    query,
+    'SELECT "f1", "f2", min("f0"), min("f3 / f0") FROM "table" ' +
       'WHERE "f1" = $1 GROUP BY "f1", "f2"'
   );
   test.strictSame(params.build(), [3]);
