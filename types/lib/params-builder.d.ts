c7e2157d027cd7e63bf083a4c3642a4fbad9d2b1
Denys Otrishko <shishugi@gmail.com>
2020-08-20 20:46:16 +0300

    fixup! feat: add TS typings

--- types/lib/params-builder.d.ts
+++ types/lib/params-builder.d.ts
@@ -1,7 +1,12 @@
-export class ParamsBuilder<P = any> {
+export interface ParamsBuilderOptions {}
+
+export class ParamsBuilder<
+  P = any,
+  O extends ParamsBuilderOptions = ParamsBuilderOptions
+> {
   // Add passed value to parameters
   // Returns a string name to put in an sql query
-  add(value: any): string;
+  add(value: any, options?: O): string;
 
   // Generic building method that must return the parameters object
   build(): P;
